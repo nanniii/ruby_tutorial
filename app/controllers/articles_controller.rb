7c994430ed39bafdc9af114ef784717599838bb1
will <wenderybill@yahoo.com>
2018-04-17 16:23:54 -0400

    Assignment Commit up until sec 5.8

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,36 @@
+class ArticlesController < ApplicationController
+    
+    def index
+        @articles = Article.all
+    end
+    
+    
+    
+    def show 
+        @article = Article.find(params[:id])
+    end
+    
+    
+    def new
+    end
+    
+    def create
+        @article = Article.new(article_params)
+        
+        @article.save
+        redirect_to @article
+        
+        @article = Article.new(params.require(:article).permit(:title,:text))
+    end
+    
+    
+    
+    
+    private 
+    def article_params
+        params.require(:article).permit(:title, :text) 
+    end
+    
+    
+    
+end
